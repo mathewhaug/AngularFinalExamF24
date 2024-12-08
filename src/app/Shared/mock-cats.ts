4a3bc730cba37072f9d574c340f4a1caf64c4a67
matt <mhaug@stclaircollege.ca>
2024-12-08 14:34:46 -0500

    Added More Cats

--- src/app/Shared/mock-cats.ts
+++ src/app/Shared/mock-cats.ts
@@ -5,4 +5,6 @@ export const CAT_ARRAY: Cat[] = [
   { id: 2, name: 'Loki', breed: 'Domestic Short Hair', age: 2  , isGoodKitty: true, picture: ["Loki/Loki.jpg", "Loki/Loki1.jpg", "Loki/Loki2.jpg", "Loki/Loki3.jpg", "Loki/Loki4.jpg", "Loki/Loki5.jpg"]},
   { id: 3, name: 'Echo', breed: 'Domestic Short Hair', age: 1  , isGoodKitty: true, picture: ["Echo/Echo.jpg","Echo/Echo2.jpg","Echo/Echo3.jpg","Echo/Echo4.jpg","Echo/Echo5.jpg","Echo/Echo6.jpg","Echo/Echo1.jpg"]},
   { id: 4, name: 'Baymax', breed: 'Domestic Short Hair', age: 3  , isGoodKitty: true, picture: ["Baymax/Baymax.jpg", "Baymax/Baymax1.jpg", "Baymax/BayMaxPrettyGuy.jpg", "Baymax/BaymarThePirate.jpg"]},
+  {id: 5, name: 'Huxley', breed: 'Domestic Short Hair', age: 9, isGoodKitty: true, picture:["Huxley/Huxley2.jpg","Huxley/Huxley1.jpg","Huxley/Huxley.jpg","Huxley/Huxley3.jpg","Huxley/Huxley4.jpg","Huxley/Huxley5.jpg",]},
+  {id: 6, name: 'Andy', breed: 'Domsestic Short Hair', age: 5, isGoodKitty: true, picture: ["Andy/Andy3.webp","Andy/Andy.jpg","Andy/Andy2.jpg","Andy/Andy4.webp","Andy/Andy6.webp","Andy/Andy5.webp"]}
 ];
