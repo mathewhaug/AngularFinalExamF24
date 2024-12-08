d0a82a4eba2e4e20a4057328c6dda5c5b9c05ccb
matt <mhaug@stclaircollege.ca>
2024-12-07 20:17:14 -0500

    Started File Complete

--- src/app/Shared/mock-cats.ts
+++ src/app/Shared/mock-cats.ts
@@ -1,8 +1,8 @@
 import { Cat } from './cats';
 //Static data of cats
 export const CAT_ARRAY: Cat[] = [
-  { id: 1, name: 'Smokey', breed: 'Domestic Short Hair', age: 12 , isGoodKitty: true, picture: ["Smokey.jpg","smokey.jpg","smokeyParty.jpg"]},
-  { id: 2, name: 'Loki', breed: 'Domestic Short Hair', age: 2  , isGoodKitty: true, picture: ["Loki.jpg"]},
-  { id: 3, name: 'Echo', breed: 'Domestic Short Hair', age: 1  , isGoodKitty: true, picture: ["Echo.jpg"]},
-  { id: 4, name: 'Baymax', breed: 'Domestic Short Hair', age: 3  , isGoodKitty: true, picture: ["Baymax.jpg", "Baymax1.jpg", "BayMaxPrettyGuy.jpg", "BaymarThePirate.jpg"]},
+  { id: 1, name: 'Smokey', breed: 'Domestic Short Hair', age: 12 , isGoodKitty: true, picture: ["Smokey/Smokey.jpg","Smokey/smokey.jpg","Smokey/smokeyParty.jpg"]},
+  { id: 2, name: 'Loki', breed: 'Domestic Short Hair', age: 2  , isGoodKitty: true, picture: ["Loki/Loki.jpg", "Loki/Loki1.jpg", "Loki/Loki2.jpg", "Loki/Loki3.jpg", "Loki/Loki4.jpg", "Loki/Loki5.jpg"]},
+  { id: 3, name: 'Echo', breed: 'Domestic Short Hair', age: 1  , isGoodKitty: true, picture: ["Echo/Echo.jpg","Echo/Echo2.jpg","Echo/Echo3.jpg","Echo/Echo4.jpg","Echo/Echo5.jpg","Echo/Echo6.jpg","Echo/Echo1.jpg"]},
+  { id: 4, name: 'Baymax', breed: 'Domestic Short Hair', age: 3  , isGoodKitty: true, picture: ["Baymax/Baymax.jpg", "Baymax/Baymax1.jpg", "Baymax/BayMaxPrettyGuy.jpg", "Baymax/BaymarThePirate.jpg"]},
 ];
